a2571fa8229bb8a56eae352587a1c89c556e163d
Christian Claro <88514581+sistemaec@users.noreply.github.com>
2022-02-11 23:35:05 -0500

    Actualizacion de servidor de db en godad

--- app/config/config.php
+++ app/config/config.php
@@ -10,10 +10,10 @@ defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 return new \Phalcon\Config([
     'database' => [
         'adapter'     => 'Mysql',
-        'host'        => 'remotemysql.com', /* localhost */
-        'username'    => 'xGFTTKOdxu', /* root */
-        'password'    => 'qMLoy0TBzM', /* jmbg1977 */
-        'dbname'      => 'xGFTTKOdxu', /* pointerapp */
+        'host'        => '107.180.27.234', //remotemysql.com
+        'username'    => 'daikoapp', // aRmvBqS4iI poinspve_iceq
+        'password'    => 'bp#pn2g~nFm-', // sZbk7mlQ3T Caricatur@55
+        'dbname'      => 'daikores', // aRmvBqS4iI poinspve_ancris
         'charset'     => 'utf8',
         'port'        => 3306
     ],
